207633de1a3e8c9f2af549f5df51e1413e8a8495
juansenita <alexjuan090900@gmail.com>
2024-07-01 12:01:56 +0700

    ini file pertama

---
+++ lat1-juan.php
@@ -0,0 +1,11 @@
+<?php
+echo "hai dunia";
+//perintah untuk add atau tambah
+//1. git add . atau git add nama_folder/file
+//perintah untuk commit atau komentar
+//2. git commit -m "komentar nya apa"
+//perintah untuk upload atau push
+//3. git push -u origin master
+// push adalah perintah, origin adalah nama remote
+//master adalah nama branch
+?>
